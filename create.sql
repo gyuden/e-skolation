7cc895812fdb254d9f6544ddf3296790023e1ab7
Francelle Bacud <95568653+pzelb@users.noreply.github.com>
2023-05-19 18:24:41 +0800

    para mabilis copy paste thenkas

--- create.sql
+++ create.sql
@@ -1,28 +1,104 @@
-CREATE DATABASE eskolation;
-
 CREATE TABLE Student (
-studentID INTEGER PRIMARY KEY,
+studentID TEXT,
+portfolioID INTEGER, 
 firstName TEXT,
 middleName TEXT,
 lastName TEXT,
-yearEnrolled YEAR
+yearEnrolled YEAR,
+PRIMARY KEY (studentID, portfolioID),
+FOREIGN KEY (portfolioID) REFERENCES Portfolio(portfolioID)
 );
 
 CREATE TABLE Portfolio (
-portfolioID INTEGER PRIMARY KEY,
+portfolioID INTEGER,
+studentID TEXT,
 portfolioName TEXT,
 portfolioType TEXT,
-yearCreated YEAR
+yearCreated YEAR,
+PRIMARY KEY (portfolioID, studentID),
+FOREIGN KEY (studentID) REFERENCES Student(studentID)
+);
+
+CREATE TABLE Posts (
+portfolioID INTEGER,
+postID INTEGER,
+	postTitle TEXT,
+	postDate DATE,
+	postType TEXT,
+	views SERIAL,
+	comments INTEGER,
+	PRIMARY KEY (portfolioID, postID),
+FOREIGN KEY (portfolioID) REFERENCES Portfolio(portfolioID)
+);
+
+CREATE TABLE Contains (
+	portfolioID INTEGER,
+	postID INTEGER,
+	PRIMARY KEY (portfolioID, postID),
+	FOREIGN KEY (portfolioID) REFERENCES Portfolio(portfolioID),
+	FOREIGN KEY (postID) REFERENCES Posts(postID)
 );
 
 CREATE TABLE Program (
-programID INTEGER PRIMARY KEY,
-programName TEXT
+programID TEXT,
+programName TEXT,
+PRIMARY KEY (programID)
 );
 
-CREATE TABLE Faculty(
-facultyID INTEGER PRIMARY KEY,
+CREATE TABLE Faculty (
+facultyID INTEGER,
 firstName TEXT,
 middleName TEXT,
 lastName TEXT,
+PRIMARY KEY (facultyID)
+);
+
+CREATE TABLE TechAdmin (
+	adminID INTEGER,
+	firstName TEXT,
+	middleName TEXT,
+	lastName TEXT,
+	techRole TEXT,
+	PRIMARY KEY (adminID)
+);
+
+CREATE TABLE Visitor (
+	visitorID SERIAL,
+	PRIMARY KEY (visitorID),
+	UNIQUE (visitorID)
+);
+
+CREATE TABLE Monitors (
+	portfolioID INTEGER,
+adminID INTEGER,
+remarks TEXT,
+PRIMARY KEY (portfolioID, adminID),
+FOREIGN KEY (portfolioID) REFERENCES Portfolio(portfolioID),
+FOREIGN KEY (adminID) REFERENCES Portfolio(portfolioID)
+);
+
+CREATE TABLE Views (
+	visitorID SERIAL,
+	portfolioID INTEGER,
+	remarks TEXT,
+	PRIMARY KEY (visitorID, portfolioID),
+FOREIGN KEY (visitorID) REFERENCES Visitor(visitorID),
+FOREIGN KEY (portfolioID) REFERENCES Portfolio(portfolioID),
+UNIQUE (visitorID)
+);
+
+CREATE TABLE Takes (
+	studentID TEXT,
+	programID TEXT,
+	PRIMARY KEY (studentID, programID),
+FOREIGN KEY (studentID) REFERENCES Student(studentID),
+	FOREIGN KEY (programID) REFERENCES Program(programID)
+);
+
+CREATE TABLE Handles (
+	facultyID INTEGER,
+	programID TEXT,
+	PRIMARY KEY (facultyID, programID),
+FOREIGN KEY (facultyID) REFERENCES Faculty(facultytID),
+	FOREIGN KEY (programID) REFERENCES Program(programID)
 );
